3aa514f71e39bab035e90582b15e47cc61cb5137
gargeem29 <gargeem026@gmail.com>
2024-11-16 23:04:34 +0530

    commit

--- server/src/utils/db.js
+++ server/src/utils/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
-const dbSrc = 'mongodb://127.0.0.1:27017/car-management'
+// const dbSrc = 'mongodb://127.0.0.1:27017/car-management'
+const dbSrc = 'mongodb+srv://gargeem026:wu5CRjcLnfvNIzKK@cluster0.dizl9.mongodb.net/car-management'
 mongoose.set('strictQuery', false)
 
 async function db() {
